a497326d33507fa0bd03d3aab583f04e95494b60
Kuralamuthan Kathirvelan <kuralamuthan300@gmail.com>
2020-04-28 00:05:24 +0530

    File uploaded successfully

--- test/upload.php
+++ test/upload.php
@@ -1,5 +1,4 @@
 <?php
-//File upload
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -38,26 +37,4 @@ if ($uploadOk == 0) {
         echo "Sorry, there was an error uploading your file.";
     }
 }
-
-
-
-
-
-
-
-require("connection.php");
-$formdata = array($_POST["hint"],$_POST["ques1"],$_POST["ques2"],$_POST["ques3"],$_POST["ques4"],$_POST["ques5"],$_POST["ques6"],$_POST["ques7"],$_POST["ques8"],$_POST["ques9"],$_POST["ques10"],$_POST["ques11"],$_POST["ques12"],$_POST["ques13"],$_POST["ques14"],basename( $_FILES["fileToUpload"]["name"]),$_POST["phone"]);
-$sql = "INSERT INTO `response` (`solved`, `hint`, `ques1`, `ques2`, `ques3`, `ques4`, `ques5`, `ques6`, `ques7`, `ques8`, `ques9`, `ques10`, `ques11`, `ques12`, `ques13`, `ques14`, `photo`, `phone`) VALUES ('0',";
-  //INSERT INTO `response` (`solved`, `hint`, `ques1`, `ques2`, `ques3`, `ques4`, `ques5`, `ques6`, `ques7`, `ques8`, `ques9`, `ques10`, `ques11`, `ques12`, `ques13`, `ques14`, `photo`, `phone`) VALUES ('0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
-  for ($x = 0; $x < count($formdata); $x++) {
-    $sql = $sql."'".$formdata[$x]."',";
-    if($x == count($formdata)-1)
-    {
-        $sql = substr($sql,0,strlen($sql)-1);
-        $sql = $sql.")";
-    }
-}
-
-echo $sql;
-
 ?>
